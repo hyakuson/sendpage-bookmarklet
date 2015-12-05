19e12d7d4fed9c72ea58744f8bc86d774340b15b
Ryohei Hyakusoku <ryoheihyakusan@gmail.com>
2015-12-06 01:26:25 +0900

    first commit

---
+++ bookmarklet.js
@@ -0,0 +1,17 @@
+javascript: (function () {
+    'use strict';
+    var scriptTag;
+    scriptTag = document.createElement('script');
+    scriptTag.src = "https://cdn.mlkcca.com/v2.0.0/milkcocoa.js";
+    scriptTag.onload = function () {
+        var mk = new window.MilkCocoa('hotic23lr8n.mlkcca.com'),
+            ds = mk.dataStore('website'),
+            memo = window.prompt("メモ", "");
+        ds.push({
+            "title": document.title,
+            "url": location.href,
+            "memo": memo
+        });
+    };
+    document.body.appendChild(scriptTag);
+}());
