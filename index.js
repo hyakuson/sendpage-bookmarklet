25f47493e47ec11047cd14d33a4d4b10413b7ec9
百束遼平 <hyakusoku@hyakutabaryouhei-no-MacBook-Pro.local>
2015-12-18 17:20:06 +0900

    送られたページを表示するページを作成

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 (function () {
     'use strict';
 
-    var mk, ds;
-
-    document.getElementById('send_btn').addEventListener('click', function () {
-        var appid = document.getElementById('appid').value;
-
-        mk = new window.MilkCocoa(appid + '.mlkcca.com');
+    var mk = new window.MilkCocoa('maxihu0p5d3.mlkcca.com'),
         ds = mk.dataStore('website');
 
-        ds.stream().size(100).next(function (err, data) {
-            var tbody = document.getElementById('tbody');
+    ds.stream().size(100).next(function (err, data) {
+        var tbody = document.getElementById('tbody');
 
-            data.forEach(function (row, index) {
-                var tr = tbody.insertRow(index),
-                    td_title = tr.insertCell(0),
-                    td_url = tr.insertCell(1),
-                    td_memo = tr.insertCell(2);
-                td_title.textContent = row.value.title;
-                td_url.textContent = row.value.url;
-                td_memo.textContent = row.value.memo;
-                tr.appendChild(td_title);
-                tr.appendChild(td_url);
-                tr.appendChild(td_memo);
+        data.forEach(function (row, index) {
+            var tr = tbody.insertRow(index),
+                td_title = tr.insertCell(0),
+                td_url = tr.insertCell(1),
+                td_memo = tr.insertCell(2),
+                a_url = document.createElement('a');
+            td_title.textContent = row.value.title;
+            a_url.setAttribute('href', row.value.url);
+            a_url.setAttribute('target', '_blank');
+            a_url.appendChild(document.createTextNode(row.value.url));
+            td_url.appendChild(a_url);
+            td_memo.textContent = row.value.memo;
+            tr.appendChild(td_title);
+            tr.appendChild(td_url);
+            tr.appendChild(td_memo);
 
-                tbody.appendChild(tr);
-            });
+            tbody.insertBefore(tr, tbody.firstChild);
         });
-
-
     });
 }());
